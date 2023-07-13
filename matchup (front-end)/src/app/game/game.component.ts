41773f9c2841c8c0bd8fa5ea67785ca24aeb2a92
CamiloTarkowski <camilo.tarkowski@gmail.com>
2023-07-13 06:17:05 -0400

    agregando 'ver caracteres por id de un juego'

--- matchup (front-end)/src/app/game/game.component.ts
+++ matchup (front-end)/src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { CharactersService } from '../services/characters.service';
 import { Game } from '../interfaces/game.interface';
 import { GamesService } from '../services/games.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -17,7 +18,9 @@ export class GameComponent implements OnInit {
 
   constructor(private gamesService: GamesService,
               private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router,
+              private characterService: CharactersService)
+              { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
@@ -30,6 +33,9 @@ export class GameComponent implements OnInit {
           this.router.navigate(['/']);
         }
       );
+    });
+    this.characterService.getAllCharacter(2).subscribe((characters) => {
+      this.characters = characters;
     });
   }
 
